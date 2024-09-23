ee4d099605686154b51ebca6b33c96bcbeaf7a75
Raulzera08 <160195866+Raulzera08@users.noreply.github.com>
2024-09-23 10:53:39 -0300

    jhhgdkha

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ function areatriangulo(base,altura){
 }
 console.log(areatriangulo(5,5));
 
-let nome=parseInt(prompt('digite seu primeiro numero'/));
-let nome=parseInt(prompt('digete seu segundo numero'/));
-let nome=parseInt(prompt('digete seu terceiro numero'/));
+let valor=parseInt(prompt('digite seu primeiro numero'/));
+let valor=parseInt(prompt('digete seu segundo numero'/));
+let valor=parseInt(prompt('digete seu terceiro numero'/));
 function media(valor1,valor2,valor3){
     return (valor1+valor2+valor3)/3;
 }
